48afb945efef9b5247cf8c65093942697f55f0a4
Tcholly <roberti.gionata@protonmail.ch>
2022-10-19 20:50:07 +0200

    Retabbet to stay consistent with style

--- ResourceManager/src/Utils/Logger.h
+++ ResourceManager/src/Utils/Logger.h
@@ -4,15 +4,15 @@
 #include <fmt/core.h>
 
 #ifdef _DEBUG
-    #define LOG_INFO(msg, ...)  Logger::Info(msg, ##__VA_ARGS__)
-    #define LOG_WARN(msg, ...)  Logger::Warn(msg, ##__VA_ARGS__)
-    #define LOG_ERROR(msg, ...) Logger::Error(msg, ##__VA_ARGS__)
-    #define LOG_DEBUG(msg, ...) Logger::Debug(msg, ##__VA_ARGS__)
+	#define LOG_INFO(msg, ...)  Logger::Info(msg, ##__VA_ARGS__)
+	#define LOG_WARN(msg, ...)  Logger::Warn(msg, ##__VA_ARGS__)
+	#define LOG_ERROR(msg, ...) Logger::Error(msg, ##__VA_ARGS__)
+	#define LOG_DEBUG(msg, ...) Logger::Debug(msg, ##__VA_ARGS__)
 #else
-    #define LOG_INFO(msg, ...)
-    #define LOG_WARN(msg, ...)
-    #define LOG_ERROR(msg, ...)
-    #define LOG_DEBUG(msg, ...)
+	#define LOG_INFO(msg, ...)
+	#define LOG_WARN(msg, ...)
+	#define LOG_ERROR(msg, ...)
+	#define LOG_DEBUG(msg, ...)
 #endif
 
 
@@ -24,31 +24,31 @@
 
 namespace Logger
 {
-    template <typename... T>
-    void Debug(std::string message, T&&... args)
-    {
-        std::string finalMessage = fmt::format(message, args...);
-        std::cout << "[DEBUG]: " << finalMessage << '\n';
-    }
+	template <typename... T>
+	void Debug(std::string message, T&&... args)
+	{
+		std::string finalMessage = fmt::format(message, args...);
+		std::cout << "[DEBUG]: " << finalMessage << '\n';
+	}
     
-    template <typename... T>
-    void Info(std::string message, T&&... args)
-    {
-        std::string finalMessage = fmt::format(message, args...);
-        std::cout << CONSOLE_GREEN "[INFO]: " << finalMessage << CONSOLE_WHITE << '\n';
-    }
+	template <typename... T>
+	void Info(std::string message, T&&... args)
+	{
+		std::string finalMessage = fmt::format(message, args...);
+	std::cout << CONSOLE_GREEN "[INFO]: " << finalMessage << CONSOLE_WHITE << '\n';
+	}
     
-    template <typename... T>
-    void Warn(std::string message, T&&... args)
-    {
-        std::string finalMessage = fmt::format(message, args...);
-        std::cout << CONSOLE_YELLOW "[WARNING]: " << finalMessage << CONSOLE_WHITE << '\n';
-    }
+	template <typename... T>
+	void Warn(std::string message, T&&... args)
+	{
+		std::string finalMessage = fmt::format(message, args...);
+		std::cout << CONSOLE_YELLOW "[WARNING]: " << finalMessage << CONSOLE_WHITE << '\n';
+	}
 
-    template <typename... T>
-    void Error(std::string message, T&&... args)
-    {
-        std::string finalMessage = fmt::format(message, args...);
-        std::cerr << CONSOLE_RED "[ERROR]: " << finalMessage << CONSOLE_WHITE << '\n';
-    }
+	template <typename... T>
+	void Error(std::string message, T&&... args)
+	{
+		std::string finalMessage = fmt::format(message, args...);
+		std::cerr << CONSOLE_RED "[ERROR]: " << finalMessage << CONSOLE_WHITE << '\n';
+	}
 } // namespace Logger
